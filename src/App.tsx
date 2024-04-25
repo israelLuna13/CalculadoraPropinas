420f9111848d1bf9f2d6fec7faf4624afa3f1032
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-04-24 23:05:23 -0600

    se agrego la seleccion de la comida y el subtotal

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,45 @@
-import MenuItem from "./components/MenuItem"
-import { menuItems } from "./data/db"
-
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/uderOrder";
+import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
 function App() {
+  const {order,addItem,removeItem} = useOrder()
   return (
     <>
-    <header className=" bg-teal-400 py-5">
-      <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      <header className=" bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-5">
+            {menuItems.map((item) => (
+              <MenuItem 
+              key={item.id}  
+              item={item}
+              addItem={addItem}
+              
+              />
+            ))}
+          </div>
+        </div>
 
-
-    </header>
-    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-      <div>
-
-        <h2>Menu</h2>
-
-        {menuItems.map(item => (
-          <MenuItem
-          key={item.id}
-          item={item}
-          />
-        ))}
-
-
-      </div>
-
-      <div>
-        <h2>Consumo</h2>
-
-      </div>
-      
-    </main>
-
-
+        <div className="border border-dashed border-slate-300 p-5 rounded-ñg space-y-10">
+       
+       <OrderContents
+       order={order}
+       removeItem={removeItem}
+       />
+       <OrderTotals
+       order = {order}
+       />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,19 @@
+import type { MenuItem} from "../types"
+type MenuItemProps = {
+  item:MenuItem
+  addItem: (item:MenuItem) => void
 
-export default function MenuItem() {
+}
+
+export default function MenuItem({item,addItem}:MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button
+    className="border-2 border-teal-400
+     hover:bg-teal-200 w-full p-3 flex justify-between"
+     onClick={()=>addItem(item)}
+    >
+    <p>{item.name}</p>
+    <p className="font-blanck">{item.price}</p>
+    </button>
   )
 }
