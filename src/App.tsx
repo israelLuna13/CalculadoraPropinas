07f4e1bdd3e4b74fb7945d7fd0045e929acce4bd
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-04-25 12:25:07 -0600

    Primera parte terminada del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
-import useOrder from "./hooks/uderOrder";
+import useOrder from "./hooks/userOrder";
 import OrderContents from "./components/OrderContents";
 import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
+
 function App() {
-  const {order,addItem,removeItem} = useOrder()
+
+  const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder();
+
   return (
     <>
       <header className=" bg-teal-400 py-5">
@@ -12,30 +16,55 @@ function App() {
           Calculadora de Propinas y Consumo
         </h1>
       </header>
+
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div className="p-5">
           <h2 className="text-4xl font-black">Menu</h2>
           <div className="space-y-3 mt-5">
+           
             {menuItems.map((item) => (
+               
               <MenuItem 
-              key={item.id}  
-              item={item}
-              addItem={addItem}
-              
-              />
+               key={item.id}
+               item={item} 
+               addItem={addItem} />
             ))}
+            
           </div>
         </div>
 
         <div className="border border-dashed border-slate-300 p-5 rounded-ñg space-y-10">
-       
-       <OrderContents
-       order={order}
-       removeItem={removeItem}
-       />
-       <OrderTotals
-       order = {order}
-       />
+
+          {order.length  ?(
+            <>
+            
+          <OrderContents 
+          order={order} 
+          removeItem={removeItem} />
+
+          <TipPercentageForm
+           setTip={setTip}
+           tip = {tip}
+           />
+
+          <OrderTotals
+           order={order}
+           tip = {tip}
+           placeOrder = {placeOrder}
+
+           />
+            </>
+
+          ) : (
+            <p className="text-center">La order esta vacia</p>
+
+          )
+        
+        }
+
+
+
+
         </div>
       </main>
     </>
